a30703d79e4e94bcd36d79ae474d5223009608ce
Naufal Ilham Ramadhan <111553406+nnnnaufalir@users.noreply.github.com>
2023-01-25 19:06:30 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+
+# path of video
+vidcap = cv2.VideoCapture("D:/NAUFAL/Project - UNNES/AI - Pak Anan/Refrensi/Convert mp4 to jpg/plat.mp4")
+success, image = vidcap.read()
+
+
+count = 0 
+while vidcap.isOpened():
+    success, image = vidcap.read()
+    cv2.imwrite("D:/NAUFAL/Project - UNNES/AI - Pak Anan/Refrensi/Convert mp4 to jpg/frame/%d.png" % count, image )
+    count+=1
